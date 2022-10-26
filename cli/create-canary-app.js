aef0babdfd0e7a1eb84b68085bfd157339817192
Toon Janssens <toon@codibri.be>
2022-10-26 15:52:34 +0200

    move to tempate

--- cli/create-canary-app.js
+++ cli/create-canary-app.js
@@ -1,3 +1,40 @@
+#!/usr/bin/env node
+
+const downloader = require('github-download-directory');
+
+const { execSync } = require('child_process')
+
+const runCommand = command => {
+    try {
+        execSync(`${command}`, { stdio: 'inherit'});
+    } catch (e) {
+        console.error(e);
+        return false
+    }
+
+    return true
+};
+
+
 const repoName = process.argv[2]
 
-const gitCheckoutCommand = `git clone --depth 1 https://github.com/toonjanssens/create-canary-poc.git ${repoName}`
+// console.log(`Cloning the repository with name ${repoName}`)
+
+// const checkout = runCommand(`git clone --depth 1 https://github.com/toonjanssens/create-canary-poc.git ${repoName}`);
+// if (!checkout) process.exit(-1);
+
+console.log(`Downloading the template into ${repoName}`)
+
+
+downloader.download('toonjanssens', 'create-canary-poc', 'template').then(console.log, (e) => {
+    console.error(e)
+    process.exit(-1)
+});
+
+// console.log(`Installing dependencies`)
+
+// const installDeps = runCommand(`cd ${repoName} && yarn install`);
+// if (!installDeps) process.exit(-1);
+
+// console.log(`Install your needed plugins and start with the following command`);
+// console.log(`cd ${repoName} && yarn start`)
